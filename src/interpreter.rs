7c008a577d58af1c04331f58d16dca9dd2d163bb
Russell Mirabelli <russell@mirabellifamily.org>
2018-02-25 18:56:25 -0600

    cleanup

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -8,36 +8,7 @@ use environment_parser::*;
 use std::collections::HashMap;
 
 pub fn interpret_with_environment(s:String, e: Environment) -> i32 {
-
-    // apply each element in environment to the string
-    // this is *applicitave form*, not *normal form*.
-    // Scheme is, by definition, applicative form.
-    // Also note that this is not very smart... this should
-    // be done by the parser, not with string replacement
-    let mut s = s;
-    for key in e.variables.keys() {
-        //s = s.replace(key,e.variables.get(key).unwrap());
-    }
-    interpret(s)
-
-}
-
-fn replace_from_environment(s:String, e:&Environment) -> String {
-    for key in e.variables.keys() {
- //       if key == &s { return e.variables.get(key).unwrap().to_string(); }
-    }
-    return s;
-}
-
-pub fn interpret_with_environment_2(s:String, e: Environment) -> i32 {
-    let original_tokens = tokenize(&s);
-    let mut tokens = Vec::<Token>::new();
-    for token in original_tokens {
-        match token {
-//            Token::Constant(a) => tokens.push(Token::Constant(replace_from_environment(a,&e))),
-            _ => tokens.push(token),
-        }
-    }
+    let mut tokens = tokenize(&s);
     let expression = parse(&mut tokens, &e);
     let result = evaluate(&expression);
     return result.unwrap();
@@ -46,11 +17,10 @@ pub fn interpret_with_environment_2(s:String, e: Environment) -> i32 {
 pub fn interpret_with_environment_string(s:String, e:String) -> i32 {
     let mut env_tokens = tokenize(&e);
     let env = parse_to_environment(&mut env_tokens);
-    interpret_with_environment_2(s,env)
+    interpret_with_environment(s,env)
 }
 
 pub fn interpret(s: String) -> i32 {
-
     let hash: HashMap<String, Expression> = HashMap::new();
     let env = Environment { variables: hash };
     let mut tokens = tokenize(&s);
